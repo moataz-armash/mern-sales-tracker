95902002a906186d75a3404d582403c96819303f
moataz-armash <mo3taz314@gmail.com>
2024-11-24 05:36:55 +0300

    Use environment variables for Firebase credentials

--- backend/firebase/firestore.js
+++ backend/firebase/firestore.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 
 // Load Firebase service account key from file
-const serviceAccount = require("../firebase-key.json");
+const firebaseCredentials = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({
